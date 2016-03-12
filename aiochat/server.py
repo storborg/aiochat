970a071fdf0aed2dd9e0571361e56957a69133b1
Scott Torborg <storborg@gmail.com>
2016-03-12 00:02:26 -0800

    Allow changing nicknames

--- aiochat/server.py
+++ aiochat/server.py
@@ -6,13 +6,15 @@ import websockets
 from websockets.exceptions import ConnectionClosed
 
 
-connected = set()
+connected = {}
 
 
 async def broadcast(origin, msg):
+    print('ALL > %s' % msg)
     for ws in connected:
         if ws != origin:
-            await ws.send('%s' % msg)
+            origin_name = connected[origin]
+            await ws.send('%s: %s' % (origin_name, msg))
 
 
 async def clock():
@@ -21,11 +23,30 @@ async def clock():
     return 'The time is ' + time.strftime('%H:%M:%S', time.localtime())
 
 
+async def command(origin, msg):
+    sp = msg.strip().split(None, 1)
+    if len(sp) > 1:
+        cmd, arg = sp
+    else:
+        cmd = sp[0]
+    assert cmd.startswith('/')
+    cmd = cmd[1:]
+    if cmd == 'nick':
+        connected[origin] = arg
+        await origin.send('** You are now known as %s **' % arg)
+    else:
+        await origin.send('** Unknown command. **')
+
+
 async def handler(websock, path):
     global connected
-    print('new connection')
-    connected.add(websock)
+
+    addr = websock.remote_address[0]
+    connected[websock] = name = 'Anonymous %d' % id(websock)
+    print('New connection from %s, known as %s' % (addr, name))
+
     try:
+        await websock.send('** You are known as %s **' % name)
         while True:
             listener_task = asyncio.ensure_future(websock.recv())
             clock_task = asyncio.ensure_future(clock())
@@ -36,23 +57,27 @@ async def handler(websock, path):
             if listener_task in done:
                 msg = listener_task.result()
                 msg = msg.strip()
-                print('< %r' % msg)
-                await broadcast(websock, msg)
-                await websock.send('** sent message **')
+                print('%s < %s' % (addr, msg))
+                if msg.startswith('/'):
+                    await command(websock, msg)
+                else:
+                    await broadcast(websock, msg)
+                    print('%s > ** sent message **' % addr)
+                    await websock.send('** sent message **')
             else:
                 listener_task.cancel()
 
             if clock_task in done:
                 msg = clock_task.result()
-                print('> %r' % msg)
-                await broadcast(None, msg)
+                print('%s > %s' % (addr, msg))
+                await websock.send('Server: ' + msg)
             else:
                 clock_task.cancel()
 
     except ConnectionClosed:
-        print('disconnected')
+        print('Disconnected')
     finally:
-        connected.remove(websock)
+        del connected[websock]
 
 
 def main(args=sys.argv):
